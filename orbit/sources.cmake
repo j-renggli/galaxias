ed8abd93e73f65ec9ecfc3c4a797e21ba9919dbe
Julien Renggli <jrenggli@gmail.com>
2023-01-23 10:33:59 +0100

    Very basic initial structure

---
+++ orbit/sources.cmake
@@ -0,0 +1,16 @@
+get_filename_component(library_name ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+set(objects_name ${library_name}_object)
+
+set(library_src
+    include/${library_name}/centerofmass.h
+    include/${library_name}/position.h
+)
+
+set(object_library_src
+    src/position.cpp
+    src/position.h
+)
+
+# Trick to show the file sources.cmake in the IDE
+list(APPEND library_src sources.cmake)
+set_source_files_properties(sources.cmake PROPERTIES HEADER_FILE_ONLY TRUE)
