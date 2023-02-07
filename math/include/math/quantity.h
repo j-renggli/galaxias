ed982a603256ba2f79c0a1070cd23accff3e2a6e
Julien Renggli <jrenggli@gmail.com>
2023-02-07 11:31:54 +0100

    Rule of 5, comparison operators

--- math/include/math/quantity.h
+++ math/include/math/quantity.h
@@ -20,41 +20,51 @@ struct Quantity
         : val_{value}
     {
     }
+    constexpr Quantity(const Quantity&) = default;
+    Quantity(Quantity&& rhs) noexcept = default;
+    ~Quantity() = default;
+
+    Quantity& operator=(const Quantity&) = default;
+    Quantity& operator=(Quantity&&) noexcept = default;
+
+    /// Comparison operators
+    bool operator<(const Quantity& rhs) const { return val_ < rhs.val_; }
+    bool operator<(double value) const { return val_ < value; }
+    bool operator<=(const Quantity& rhs) const { return val_ <= rhs.val_; }
+    bool operator<=(double value) const { return val_ <= value; }
+    bool operator==(const Quantity& rhs) const { return val_ == rhs.val_; }
+    bool operator==(double value) const { return val_ == value; }
+    bool operator>=(const Quantity& rhs) const { return val_ >= rhs.val_; }
+    bool operator>=(double value) const { return val_ >= value; }
+    bool operator>(const Quantity& rhs) const { return val_ > rhs.val_; }
+    bool operator>(double value) const { return val_ > value; }
 
     /// Unary minus
     Quantity operator-() const { return Quantity(-val_); }
 
     /// Multiply with another quantity
-    template <class U2,
-              class Out = typename unit::MultiplyUnit<U, U2>::value_type>
+    template <class U2, class Out = typename unit::MultiplyUnit<U, U2>::value_type>
     Quantity<Out> operator*(const Quantity<U2>& rhs) const
     {
         return Quantity<Out>(val_ * rhs.value());
     }
 
     /// Divide by another quantity
-    template <class U2,
-              class Out = typename unit::DivideUnit<U, U2>::value_type>
+    template <class U2, class Out = typename unit::DivideUnit<U, U2>::value_type>
     Quantity<Out> operator/(const Quantity<U2>& rhs) const
     {
         return Quantity<Out>(val_ / rhs.value());
     }
 
     /// Elevate to the nth power
-    template <
-        int N,
-        int D = 1,
-        class Out = typename unit::PowerUnit<U, std::ratio<N, D>>::value_type>
+    template <int N, int D = 1, class Out = typename unit::PowerUnit<U, std::ratio<N, D>>::value_type>
     Quantity<Out> pow() const
     {
-        return Quantity<Out>(
-            std::pow(val_, static_cast<double>(N) / static_cast<double>(D)));
+        return Quantity<Out>(std::pow(val_, static_cast<double>(N) / static_cast<double>(D)));
     }
 
     /// Take the positive result of the nth root (helper function)
-    template <
-        int N,
-        class Out = typename unit::PowerUnit<U, std::ratio<1, N>>::value_type>
+    template <int N, class Out = typename unit::PowerUnit<U, std::ratio<1, N>>::value_type>
     Quantity<Out> root() const
     {
         return pow<1, N>();
